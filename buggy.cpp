3b27a636e0a3e57805d5b4d40bba8e3ad4907008
nthnbrks <nthnbrks@tamu.edu>
2024-08-27 16:35:17 +0000

    Now returns correct output

--- buggy.cpp
+++ buggy.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstring>
 
 struct Point {
     int x, y;
@@ -11,31 +10,33 @@ struct Point {
 class Shape {
     int vertices;
     Point** points;
-
-    Shape (int _vertices) {
-        vertices = _vertices;
-        points = new Point*[vertices+1];
-    }
-
-    ~Shape () {
-    }
-
-    void addPoints (/* formal parameter for unsized array called pts */) {
-        for (int i = 0; i <= vertices; i++) {
-            memcpy(points[i], &points[i%vertices], sizeof(Point));
+    
+    public:
+        Shape (int _vertices) {
+            vertices = _vertices;
+            points = new Point*[vertices+1];
         }
-    }
+
+        ~Shape () {
+        }
+
+        void addPoints (/* formal parameter for unsized array called pts */Point pts[]) {
+            for (int i = 0; i <= vertices; i++) {
+                points[i] = new Point(pts[i].x,pts[i].y);
+                points[vertices] = new Point(pts[0].x,pts[0].y);
+            }
+        }
 
     double* area () {
         int temp = 0;
-        for (int i = 0; i <= vertices; i++) {
+        for (int i = 0; i <= vertices-1; i++) {
             // FIXME: there are two methods to access members of pointers
             //        use one to fix lhs and the other to fix rhs
             int lhs = points[i]->x * points[i+1]->y;
-            int rhs = points[i+1]->x * points[i]->y;
+            int rhs = (*points[i+1]).x * (*points[i]).y;
             temp += (lhs - rhs);
         }
-        double area = abs(temp)/2.0;
+        double* area = new double(abs(temp)/2.0);
         return area;
     }
 };
@@ -43,14 +44,14 @@ class Shape {
 int main () {
     // FIXME: create the following points using the three different methods
     //        of defining structs:
-              Point tri1(0,0);
-              Point tri2(1,2);
+              Point tri1;
+              Point* tri2 = new Point(1,2);
               Point tri3(2,0);
 
     // adding points to tri
-    Point triPts[3] = {tri1, tri2, tri3};
+    Point triPts[3] = {tri1, *tri2, tri3};
     Shape* tri = new Shape(3);
-    tri.addPoints(triPts);
+    tri->addPoints(triPts);
 
     // FIXME: create the following points using your preferred struct
     //        definition:
@@ -62,7 +63,9 @@ int main () {
     // adding points to quad
     Point quadPts[4] = {quad1, quad2, quad3, quad4};
     Shape* quad = new Shape(4);
-    quad.addPoints(quadPts);
+    quad->addPoints(quadPts);
 
     // FIXME: print out area of tri and area of quad
+    printf("Area of triangle: %f\n",*(tri->area()));
+    printf("Area of quadrilateral: %f\n",*(quad->area()));
 }
